ecc1350eefb1f0c7e66e9566eabafd6684306f64
Shichang <tariad@outlook.com>
2022-11-24 22:13:09 -0800

    extend interval 2

--- src/extend_interval.py
+++ src/extend_interval.py
@@ -1,8 +1,33 @@
 from Interval import Interval
 from numba import types
+from numba.extending import typeof_impl, as_numba_type
+from numba.extending import type_callable
+
 
 class IntervalType(types.Type):
     def __init__(self):
         super(IntervalType, self).__init__(name='Interval')
 
+
 interval_type = IntervalType()
+
+
+# This is the function that Numba will call to get the type of an Interval
+# infer numba type of python object
+# runtime
+@typeof_impl.register(Interval)
+def typeof_index(val, c):
+    return interval_type
+
+
+# infer the Numba type of Python types
+# compile time
+as_numba_type.register(Interval, interval_type)
+
+
+@type_callable(Interval)
+def type_interval(context):
+    def typer(lo, hi):
+        if isinstance(lo, types.Float) and isinstance(hi, types.Float):
+            return interval_type
+    return typer
